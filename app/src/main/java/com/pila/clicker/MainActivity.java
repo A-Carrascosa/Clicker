feaca05b37e5580f07915ef2100c2ffa2f4220d1
A-Carrascosa <acarrascosa@cifpfbmoll.eu>
2021-10-22 19:38:48 +0200

    Relative layout SavedInstanceState

--- app/src/main/java/com/pila/clicker/MainActivity.java
+++ app/src/main/java/com/pila/clicker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.pila.clicker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -11,18 +13,31 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int count = 0;
+    String KEY_COUNT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(KEY_COUNT);
+            TextView showCount = findViewById(R.id.show_count);
+            showCount.setText(Integer.toString(count));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT, count);
     }
 
     public void onCount(View view) {
         count++;
         TextView showCount = findViewById(R.id.show_count);
         showCount.setText(Integer.toString(count));
-        Log.i("Count",Integer.toString(count));
+        Log.i("Count", Integer.toString(count));
     }
 
     public void onToast(View view) {
