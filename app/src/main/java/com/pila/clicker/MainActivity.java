093c1cf82038e8ddf48d00ea480b475991c73ca5
A-Carrascosa <acarrascosa@cifpfbmoll.eu>
2021-10-22 17:55:40 +0200

    Aplicacio funcionant

--- app/src/main/java/com/pila/clicker/MainActivity.java
+++ app/src/main/java/com/pila/clicker/MainActivity.java
@@ -3,12 +3,29 @@ package com.pila.clicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCount(View view) {
+        count++;
+        TextView showCount = findViewById(R.id.show_count);
+        showCount.setText(Integer.toString(count));
+        Log.i("Count",Integer.toString(count));
+    }
+
+    public void onToast(View view) {
+        Toast.makeText(this, Integer.toString(count), Toast.LENGTH_SHORT).show();
     }
 }
